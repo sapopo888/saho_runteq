624afb708bbdc7bd5244e3f9abd403a911a45b11
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-08 11:50:49 +0900

    rubocop修正

--- db/migrate/20250507132846_create_albums.rb
+++ db/migrate/20250507132846_create_albums.rb
@@ -4,7 +4,7 @@ class CreateAlbums < ActiveRecord::Migration[7.2]
       t.string :title, null: false
       t.text :description, null: false
       t.references :user, foreign_key: true
-      
+
       t.timestamps
     end
   end
