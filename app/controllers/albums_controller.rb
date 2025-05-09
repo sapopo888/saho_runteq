c7d479b339ba3db02c930401c1a5b88fad6f9697
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-09 17:19:46 +0900

    フラッシュメッセージ汎用できるように修正

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -11,9 +11,9 @@ class AlbumsController < ApplicationController
     @album = current_user.albums.build(album_params)
     if @album.save
       # ここあとで変更する
-      redirect_to albums_path, status: :see_other, notice: t('defaults.flash_message.created')
+      redirect_to albums_path, status: :see_other, notice: t('defaults.flash_message.created', item: Album.model_name.human)
     else
-      flash.now[:alert] = t('defaults.flash_message.not_created')
+      flash.now[:alert] = t('defaults.flash_message.not_created', item: Album.model_name.human)
       render :new, status: :unprocessable_entity # 作成失敗した時にエラーメッセージ表示させるために必要！
     end
 
