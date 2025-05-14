ae2ba2d5c4d5928d92b3f682ba645482ad06bacd
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-14 14:47:11 +0900

    rails gコマンドの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators.system_test = nil
+    config.generators do |g|
+      g.skip_routes true # ルーティングを自動で加えない
+      g.helper false # ヘルパーファイルを自動生成しない
+      g.test_framework nil # テストフレームワークを使わない
+    end
   end
 end
